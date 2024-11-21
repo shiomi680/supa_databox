f570e1c162dad289e900ddcf6d2bc3f34b65dbcb
genki <genki680@gmail.com>
2024-11-21 21:30:23 +0900

    page-containerに名前変更

--- databox/src/app/item/[id]/page.tsx
+++ databox/src/app/item/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ItemContentPage } from "@/components/pages/items/item-content-page";
+import { ItemContentPage } from "@/components/pages/items/item-content-page-container";
 
 export default function Item({ params }: { params: { id: string } }) {
   return <ItemContentPage revision_id={params.id} />;
